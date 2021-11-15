f78a53e54ac6b669623d4adb2e56facc683af9a6
wicky.he@bgis.com <wicky.he@bgis.com>
2021-11-15 14:22:44 +0000

    jinja practices

--- models/staging/hew_shop/stg_payments.sql
+++ models/staging/hew_shop/stg_payments.sql
@@ -3,7 +3,9 @@ id as payment_id,
 orderid as order_id,
 paymentmethod as payment_method,
 status,
-amount/1000 as amount,
+
+--amount is stored in cents, convert it to dollars
+{{cents_to_dollars('amount',4) }} as amount,
 created as created_at
 
 from {{ source ('hew_shop','payment' )}}
